1a6ded4142f645e233feee3839b3963907c58e87
Ali Parvizi <snparvizi75@gmail.com>
2019-08-10 13:24:58 +0430

    organization

--- bookstack/settings.py
+++ bookstack/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django_jalali',
     'accounting.apps.AccountingConfig',
     'tag.apps.TagConfig',
+    'organization.apps.OrganizationConfig',
     'user_management.apps.UserManagementConfig'
 ]
 
