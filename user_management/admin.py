dc72c58de426af135dee7a33539a3cea21c97091
Ali Parvizi <snparvizi75@gmail.com>
2019-08-10 13:54:13 +0430

    permissions

--- user_management/admin.py
+++ user_management/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import *
+from django.contrib.auth.models import Permission
 # Register your models here.
 
+class PermissionAdmin(admin.ModelAdmin):
+    list_display = ['name', 'content_type', 'codename']
 
 admin.site.register(UserProfile)
+admin.site.register(Permission, PermissionAdmin)
