3352e4303a1cf75f7be0b59a4ed0b59e9e868735
Ali Parvizi <snparvizi75@gmail.com>
2019-08-12 12:49:06 +0430

    Detail -> View

--- apiv1/urls.py
+++ apiv1/urls.py
@@ -4,6 +4,6 @@ from .views import *
 urlpatterns = [
     path('user-profile/create/', UserProfileCreateView.as_view()),
     path('organization/create', OrganizationCreateView.as_view()),
-    path('user-profile/retrieve/', UserProfileRetrieveDetail.as_view()),
+    path('user-profile/retrieve/', UserProfileRetrieveView.as_view()),
     # path('user-profile/retrieve/<pk>', UserProfileRetrieveView.as_view()),
 ]
